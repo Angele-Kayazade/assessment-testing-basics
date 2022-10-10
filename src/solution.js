6a4e16aaf4aa02627f9020d7b3d82542685d3dd4
Angele <anjelkayazade@gmail.com>
2022-10-09 20:42:24 -0400

    wrote partitionStudentsByScore function

--- src/solution.js
+++ src/solution.js
@@ -2,6 +2,17 @@
   Write a function that separates students into two buckets based on a given score. The end result for the "happy path" should be an array with two arrays inside of it.
 */
 
-function partitionStudentsByScore(students, score) {}
+function partitionStudentsByScore(students, score) {
+  let resultArray = []
+  let higherScores = []
+  let lowerScores = []
+
+  for (let i = 0; i<students.length; i++) {
+    (students[i].score <= score) ? lowerScores.push(students[i]) : higherScores.push(students[i]);
+  }
+
+  resultArray = [lowerScores,higherScores];
+  return resultArray;
+}
 
 module.exports = partitionStudentsByScore;
